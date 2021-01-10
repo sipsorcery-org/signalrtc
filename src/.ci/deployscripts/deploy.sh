8ee1bf6a62db03a126a9d52bb324296467838e0c
Aaron Clauson <aaron@sipsorcery.com>
2021-01-10 14:27:18 +0000

    Updated azure deploy script to replace sipdemo with devcall.

--- src/.ci/deployscripts/deploy.sh
+++ src/.ci/deployscripts/deploy.sh
@@ -1,11 +1,11 @@
-echo 'SIP demo server release deploy script starting...'
+echo 'devcall release deploy script starting...'
 pwd
 
 echo "AGENT_RELEASEDIRECTORY=$AGENT_RELEASEDIRECTORY"
 echo "BUILD_DEFINITIONNAME=$BUILD_DEFINITIONNAME"
 echo "RELEASE_PRIMARYARTIFACTSOURCEALIAS=$RELEASE_PRIMARYARTIFACTSOURCEALIAS"
 
-sudo systemctl stop sipdemo
-sudo cp -r "$RELEASE_PRIMARYARTIFACTSOURCEALIAS"/drop/* /opt/sipdemo
-sudo systemctl start sipdemo
-echo 'SIP demo server release deploy script finished.'
+sudo systemctl stop devcall
+sudo cp -r "$RELEASE_PRIMARYARTIFACTSOURCEALIAS"/drop/* /opt/devcall
+sudo systemctl start devcall
+echo 'devcall release deploy script finished.'
--- src/sipassets_mockdata.sql
+++ src/sipassets_mockdata.sql
@@ -1,4 +1,4 @@
-insert into sipdomains values ('899e48ef-1267-4b53-8a1c-476a176a4e80', 'democloud.sipsorcery.com', '*;192.168.0.50', CURRENT_TIMESTAMP);
+insert into sipdomains values ('899e48ef-1267-4b53-8a1c-476a176a4e80', 'demo.sipsorcery.com', '*;192.168.0.50', CURRENT_TIMESTAMP);
 insert into sipaccounts values ('D8E27F4E-CB6F-4B3A-A50F-9AA90350F141', '899e48ef-1267-4b53-8a1c-476a176a4e80', null, 'aaron', 'password', '0', CURRENT_TIMESTAMP);
 insert into sipaccounts values ('57F94071-43B6-4C58-A6FE-5018DC755B81', '899e48ef-1267-4b53-8a1c-476a176a4e80', null, 'user', 'password', '0', CURRENT_TIMESTAMP);
 insert into sipdialplans values (newid(), 'default', 'var inUri = uasTx.TransactionRequestURI;
